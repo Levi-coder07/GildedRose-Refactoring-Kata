03016b71c98575efbc8d5abdb1166e759502dff9
Levi <yulevja24@hotmail.com>
2022-12-19 13:55:58 -0500

    Refactoring Java

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -20,19 +20,19 @@ class GildedRose {
                 
                 }else if (items[i].quality < 50&&items[i].name.equals(CONCERT)&& items[i].sellIn < 11) {
                     items[i].quality = items[i].quality + 2;
-                           
+     
                         if (items[i].sellIn < 6 &&items[i].quality < 50) {
                             items[i].quality = items[i].quality + 1;
                             
                         }
                     
                 }else if (!items[i].name.equals(SULFURAS)) {
-                items[i].sellIn = items[i].sellIn - 1;
+                    items[i].sellIn = items[i].sellIn - 1;
                 }else if (items[i].sellIn < 0&&!items[i].name.equals(BRIE)
                     &&!items[i].name.equals(CONCERT)&&items[i].quality > 0 &&!items[i].name.equals(SULFURAS)) {
-                            items[i].quality = items[i].quality - 1;
+                    items[i].quality = items[i].quality - 1;
                 } else if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                    items[i].quality = items[i].quality + 1;
                 }else {
                     items[i].quality = 0;
                 }
